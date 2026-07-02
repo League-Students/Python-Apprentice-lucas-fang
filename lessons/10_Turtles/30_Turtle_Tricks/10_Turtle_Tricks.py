03efd621c8806ea8e2e34c42328e37608a1e25e5
League Student <student@jointheleague.org>
2026-07-02 12:42:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
@@ -13,11 +13,109 @@ Refer to previous turtle programs for examples of how to use these commands.
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup(600, 600, 0, 0)            # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
+tina.hideturtle()
 
 # Use tina.forward() and tina.left() to draw a triangle
 # Make each side of the triangle a different color with 
 # tina.pencolor()
 
 ... # Your code here
+he = 180
+window = turtle.Screen()
+window.bgcolor('black')
+for i in range(0, 0 ):
+  tina.circle(70, steps= 3)
+  tina.left(120)
+  tina.circle(-70, steps = 3)
+  tina.left(120)
+  tina.circle(70, steps = 3)
+  tina.left(1)
+  i = i + 1
+  print(i)
+
+n = 0 
+tina.color('white')
+for n in range(0, he ):
+    tina.circle(70, steps= 3)
+    tina.left(120)
+    tina.circle(-70, steps = 3)
+    tina.left(120)
+    tina.circle(70, steps = 3)
+    tina.left(1)
+    n = n + 1
+    print(n)
+    
+n = 0 
+tina.color('red')
+for n in range(0, he ):
+    tina.circle(70, steps= 3)
+    tina.left(120)
+    tina.circle(-70, steps = 3)
+    tina.left(120)
+    tina.circle(70, steps = 3)
+    tina.left(1)
+    n = n + 1
+    print(n)
+    
+n = 0 
+tina.color('orange')
+for n in range(0, he ):
+    tina.circle(70, steps= 3)
+    tina.left(120)
+    tina.circle(-70, steps = 3)
+    tina.left(120)
+    tina.circle(70, steps = 3)
+    tina.left(1)
+    n = n + 1
+    print(n)
+    
+n = 0 
+tina.color('yellow')
+for n in range(0, he ):
+    tina.circle(70, steps= 3)
+    tina.left(120)
+    tina.circle(-70, steps = 3)
+    tina.left(120)
+    tina.circle(70, steps = 3)
+    tina.left(1)
+    n = n + 1
+    print(n)
+    
+n = 0 
+tina.color('green')
+for n in range(0, he ):
+    tina.circle(70, steps= 3)
+    tina.left(120)
+    tina.circle(-70, steps = 3)
+    tina.left(120)
+    tina.circle(70, steps = 3)
+    tina.left(1)
+    n = n + 1
+    print(n)
+    
+n = 0 
+tina.color('blue')
+for n in range(0, he ):
+    tina.circle(70, steps= 3)
+    tina.left(120)
+    tina.circle(-70, steps = 3)
+    tina.left(120)
+    tina.circle(70, steps = 3)
+    tina.left(1)
+    n = n + 1
+    print(n)
+    
+n = 0 
+tina.color('purple')
+for n in range(0, he ):
+    tina.circle(70, steps= 3)
+    tina.left(120)
+    tina.circle(-70, steps = 3)
+    tina.left(120)
+    tina.circle(70, steps = 3)
+    tina.left(1)
+    n = n + 1
+    print(n)
+    
 
 turtle.exitonclick()                    # Close the window when we click on it
