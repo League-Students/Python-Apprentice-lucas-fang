8d7f4ed1014a7255f3c6a269113399d966218eb1
League Student <student@jointheleague.org>
2026-07-07 13:11:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -18,15 +18,15 @@ t = turtle.Turtle() # Create a turtle named t
 
 def make_a_shape(t):
     """Make a shape with turtle t. Make it go left or right or forward"""    
-    tina.forward(0.01)
-    
+    t.forward(0.01)
+    t.left(1)
 
 # 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
 # For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
 # The second ... in the for loop should be the number of shapes you want to make,
 # for example 100, or a list of numbers.
 
-num_shapes = ...
+num_shapes = 5
 
 for i in range(5):
     make_a_shape(t)
