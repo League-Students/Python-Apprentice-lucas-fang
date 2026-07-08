c8bdb2cf7fa73af237d12f91cbc8a3972f2e4f95
League Student <student@jointheleague.org>
2026-07-08 12:44:01 -0700

    Auto-commit: workspace changes before push

--- fractal.py
+++ fractal.py
@@ -3,6 +3,7 @@ import turtle
 tina = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(600,600)
+tina.hideturtle()
 
 def fractal_triangle(size, depth):
     if depth == 0: # base case
@@ -11,8 +12,8 @@ def fractal_triangle(size, depth):
             tina.left(120)
     else: #recursive case
         for i in range(3):
-            fractal_triangle(size/2, depth)
+            fractal_triangle(size/2, depth - 1)
 
 
-fractal_triangle(200, 1)
+fractal_triangle(200, 2)
 turtle.exitonclick()
