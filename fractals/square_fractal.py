6ecfefacc66dd5141dc6fb52131fa6775af300fc
League Student <student@jointheleague.org>
2026-07-08 13:01:18 -0700

    Automated commit

--- fractals/square_fractal.py
+++ fractals/square_fractal.py
@@ -3,12 +3,12 @@ import turtle
 tina = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(600,600)
-tina.hideturtle()
-tina.speed('fastest')
+#tina.hideturtle()
+#tina.speed('fastest')
 # Tell python to work with turtles, make tina, set screen size, hide tina, set tinas speed. 
 def fractal_triangle(size, depth):
     if depth == 0: # base case
-        for i in range(3):
+        for i in range(4):
             tina.forward(size)
             tina.left(90)
     """else: #recursive case
