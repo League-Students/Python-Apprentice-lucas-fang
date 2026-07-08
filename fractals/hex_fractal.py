f78843bddec6f08be4ed8b0bdacc57757944871c
League Student <student@jointheleague.org>
2026-07-08 13:20:22 -0700

    Automated commit

---
+++ fractals/hex_fractal.py
@@ -0,0 +1,29 @@
+import turtle
+
+tina = turtle.Turtle()
+screen = turtle.Screen()
+screen.setup(600,600)
+tina.hideturtle()
+tina.speed('fastest')
+# Tell python to work with turtles, make tina, set screen size, hide tina, set tinas speed. 
+def fractal_square(size, depth):
+    if depth == 0: # base case
+        tina.begin_fill()
+        for i in range(4):
+            tina.forward(size)
+            tina.left(90)
+        tina.end_fill()
+    else: #recursive case
+        for i in range(4):
+            fractal_square(size / 3, depth - 1)
+            tina.forward(size / 3)            
+            fractal_square(size / 3, depth - 1)
+            tina.forward(size * 2 / 3)
+            tina.left(90)
+
+#move tina to a good spot
+tina.penup()
+tina.goto(-250 , -250)
+tina.pendown()
+fractal_square(400, 4)
+turtle.exitonclick()
