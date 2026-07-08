b299b260bb8571f2c450cad4c8f7569e323f1a13
League Student <student@jointheleague.org>
2026-07-08 12:38:01 -0700

    Auto-commit: workspace changes before push

--- fractal.py
+++ fractal.py
@@ -6,7 +6,10 @@ screen.setup(600 , 600)
 # setup for fractal
 
 def fractal_triangle(size, depth):
-    if depth == 0
+    if depth == 0: # base case
+        for i in range(3):
+            tina.forward(size)
+            tina.left(120)
 
 
 turtle.exitonclick()
