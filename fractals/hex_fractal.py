26c0e8f56a44a58a8e430210cc06a3e547e3f500
League Student <student@jointheleague.org>
2026-07-09 21:06:01 -0700

    Auto-commit: workspace changes before push

--- fractals/hex_fractal.py
+++ fractals/hex_fractal.py
@@ -5,7 +5,6 @@ screen = turtle.Screen()
 screen.setup(600, 600)
 tina.hideturtle()
 tina.speed('fastest')
-# Tell python to work with turtles, make tina, set screen size, hide tina, set tina's speed.
 
 def sierpinski_hexagon(size, depth):
     if depth == 0:  # base case
@@ -19,18 +18,17 @@ def sierpinski_hexagon(size, depth):
         start_heading = tina.heading()
         for i in range(6):
             tina.penup()
-            tina.setheading(60 * i)
-            tina.forward(size / 2)
+            tina.setheading(start_heading + 60 * i)
+            tina.forward(size * 2 / 3)
             tina.pendown()
-            sierpinski_hexagon(size / 2, depth - 1)
+            sierpinski_hexagon(size / 3, depth - 1)
             tina.penup()
             tina.goto(start_x, start_y)
             tina.setheading(start_heading)
             tina.pendown()
 
-# move tina to a good spot
 tina.penup()
-tina.goto(0, -0)
+tina.goto(0, -150)
 tina.pendown()
-sierpinski_hexagon(200, 3)
+sierpinski_hexagon(300, 3)
 turtle.exitonclick()
