14f57e4c9c40215473baedd1b08037f098132cad
League Student <student@jointheleague.org>
2026-07-09 20:10:01 -0700

    Auto-commit: workspace changes before push

--- fractals/hex_fractal.py
+++ fractals/hex_fractal.py
@@ -15,14 +15,22 @@ def sierpinski_hexagon(size, depth):
             tina.left(60)
         tina.end_fill()
     else:  # recursive case
+        start_x, start_y = tina.position()
+        start_heading = tina.heading()
         for i in range(6):
+            tina.penup()
+            tina.setheading(60 * i)
+            tina.forward(size / 2)
+            tina.pendown()
             sierpinski_hexagon(size / 2, depth - 1)
-            tina.forward(size)
-            tina.left(60)
+            tina.penup()
+            tina.goto(start_x, start_y)
+            tina.setheading(start_heading)
+            tina.pendown()
 
 # move tina to a good spot
 tina.penup()
-tina.goto(-200, -150)
+tina.goto(0, -150)
 tina.pendown()
 sierpinski_hexagon(200, 3)
 turtle.exitonclick()
