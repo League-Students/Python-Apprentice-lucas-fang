09f61fb5a56bd30cd6fcb064c4f1d59ef972fe6d
League Student <student@jointheleague.org>
2026-07-10 12:59:01 -0700

    Auto-commit: workspace changes before push

--- fractals/fnaf.py
+++ fractals/fnaf.py
@@ -4,24 +4,30 @@ tina = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(500 , 500)
 tina.shape("turtle")
-
+tina_path = [1, 2, 3, 4, 5]
+tinapr
 camcolors = ["red", "black", "white","green","blue"]
 
 def opencam1():
     print("cam 1 open")
     screen.bgcolor(camcolors[0])
+
 def opencam2():
     print("cam 2 open")
     screen.bgcolor(camcolors[1])
+
 def opencam3():
     print("cam 3 open")
     screen.bgcolor(camcolors[2])
+
 def opencam4():
     print("cam 4 open")
     screen.bgcolor(camcolors[3])
+
 def opencam5():
     print("cam 5 open")
     screen.bgcolor(camcolors[4])
+
 def exitcam():
     print("cam exited")
     screen.bgcolor("yellow")
