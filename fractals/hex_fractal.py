4567a301d12b3ddd1dc561daf672620b542d881f
League Student <student@jointheleague.org>
2026-07-09 21:33:01 -0700

    Auto-commit: workspace changes before push

--- fractals/hex_fractal.py
+++ fractals/hex_fractal.py
@@ -1,4 +1,5 @@
 import turtle
+import math
 
 tina = turtle.Turtle()
 screen = turtle.Screen()
@@ -16,17 +17,26 @@ def sierpinski_hexagon(size, depth):
     else:  # recursive case
         start_x, start_y = tina.position()
         start_heading = tina.heading()
+
+        corners = []
         for i in range(6):
-#            tina.penup()
-            tina.setheading(start_heading + 60 * i)
-            tina.forward(size)
-            tina.setheading(start_heading + 60 * i + 30)
-#            tina.pendown()
+            angle = math.radians(start_heading + 60 * i)
+            cx = start_x + size * math.cos(angle)
+            cy = start_y + size * math.sin(angle)
+            corners.append((cx, cy, start_heading + 60 * i + 30))
+
+        tina.goto(corners[0][0], corners[0][1])
+        for i in range(6):
+            cx, cy, heading = corners[i]
+            tina.setheading(heading)
             sierpinski_hexagon(size / 3, depth - 1)
-#            tina.penup()
-            tina.goto(start_x, start_y)
-            tina.setheading(start_heading)
-#            tina.pendown()
+            next_i = (i + 1) % 6
+            nx, ny, _ = corners[next_i]
+            tina.setheading(tina.towards(nx, ny))
+            tina.goto(nx, ny)
+
+        tina.goto(start_x, start_y)
+        tina.setheading(start_heading)
 
 tina.penup()
 tina.goto(0, -150)
