23e8aee5a38b2824cb244ad14a9a756e67ca7e76
League Student <student@jointheleague.org>
2026-07-10 12:45:01 -0700

    Auto-commit: workspace changes before push

--- fractals/fnaf.py
+++ fractals/fnaf.py
@@ -12,9 +12,14 @@ def opencam1():
 def opencam2():
     print("cam 2 open")
     screen.bg(camcolors[1])
+def opencam3():
+    print("cam 3 open")
+    screen.bg(camcolors[2])
 
 
 screen.listen()
 screen.onkey(opencam1, "1")
 screen.onkey(opencam2, "2")
+screen.onkey(opencam2, "2")
+
 turtle.exitonclick()
